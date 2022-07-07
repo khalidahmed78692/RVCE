a9185601598d5121ee857d0b95f9ae49b9ad3275
khalidahmed78692 <108170420+khalidahmed78692@users.noreply.github.com>
2022-07-07 21:57:05 +0530

    Create ProgramThreeDFSAndBFS.c
    This program is written in c language and it implements depth first search and breadth first search algorithms.

---
+++ DAA LAB/ProgramThreeDFSAndBFS.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int count = 0, nov;
+
+void DFS(bool adj_mat[nov][nov], int nov, bool ver_mat[nov], int vtd)
+{
+    ver_mat[vtd] = true;
+    for (int i = 0; i < nov; i++)
+    {
+        if (adj_mat[vtd][i] && !ver_mat[i])
+        {
+            printf("%d ", i + 1);
+            DFS(adj_mat, nov, ver_mat, i);
+        }
+    }
+}
+
+void BFS(bool adj_mat[nov][nov], int nov, bool ver_mat[nov], int vtd)
+{
+    int queue[nov], front = 0, rear = -1, node;
+    ver_mat[vtd] = true;
+    queue[++rear] = vtd;
+    while (front <= rear)
+    {
+        node = queue[front++];
+        for (int i = 0; i < nov; i++)
+            if (adj_mat[node][i] && !ver_mat[i])
+            {
+                queue[++rear] = i;
+                ver_mat[i] = true;
+                printf("%d ", i + 1);
+            }
+    }
+}
+
+int main()
+{
+    printf("Enter the number of vertices: ");
+    scanf("%d", &nov);
+    bool adj_mat[nov][nov], ver_mat[nov];
+    int i, j;
+    printf("Enter the adjacency matrix (%d x %d elements):\n", nov, nov);
+    for (i = 0; i < nov; i++)
+        for (j = 0; j < nov; j++)
+            scanf("%d", &adj_mat[i][j]);
+    for (i = 0; i < nov; i++)
+        ver_mat[i] = false;
+    printf("\nDFS order is: ");
+    for (i = 0; i < nov; i++)
+        if (!ver_mat[i])
+        {
+            printf("%d ", i + 1);
+            DFS(adj_mat, nov, ver_mat, i);
+            count++;
+        }
+    if (count == 0)
+        printf("\nGraph is connected");
+    else
+        printf("\nGraph is not connected and there are %d components of it", count);
+    count = 0;
+    for (i = 0; i < nov; i++)
+        ver_mat[i] = false;
+    printf("\n\nBFS order is: ");
+    for (i = 0; i < nov; i++)
+        if (!ver_mat[i])
+        {
+            printf("%d ", i + 1);
+            BFS(adj_mat, nov, ver_mat, i);
+            count++;
+        }
+    if (count == 0)
+        printf("\nGraph is connected");
+    else
+        printf("\nGraph is not connected and there are %d components of it", count);
+    return 0;
+}
