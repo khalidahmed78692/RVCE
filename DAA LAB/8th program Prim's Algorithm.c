de68531186bb16bca70ccb638baa579a448d06eb
Mohammed Khalid Mohammed Minhajuddin Ansari <khalidahmed78692@gmail.com>
2022-09-17 16:02:37 +0530

    Update 8th program Prim's Algorithm.c

--- DAA LAB/8th program Prim's Algorithm.c
+++ DAA LAB/8th program Prim's Algorithm.c
@@ -55,6 +55,32 @@ int main()
 
 /*
 
+Output of the code:
+
+Enter the number of vertices: 9
+Enter 9x9=81 elements:
+0 4 0 0 0 0 0 8 0
+4 0 8 0 0 0 0 11 0
+0 8 0 7 0 4 0 0 2 
+0 0 7 0 9 14 0 0 0
+0 0 0 9 0 10 0 0 0
+0 0 4 14 10 0 2 0 0
+8 11 0 0 0 0 1 0 7
+0 0 2 0 0 0 6 7 0
+Edges included in the MST are:
+1) Edge(1,2) = 4
+2) Edge(1,8) = 8
+3) Edge(8,7) = 1
+4) Edge(7,6) = 2
+5) Edge(6,3) = 4
+6) Edge(3,9) = 2
+7) Edge(3,4) = 7
+8) Edge(4,5) = 9
+Cost of constructing MST is: 37
+
+
+
+
 Enter the number of vertices: 6
 Enter the weighted matrix of 6x6 elements
 0 3 0 0 5 6
