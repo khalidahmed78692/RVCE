eb9b670af227274e91b2fa8fdad8bec81d639c47
Mohammed Khalid Mohammed Minhajuddin Ansari <khalidahmed78692@gmail.com>
2022-09-18 23:56:00 +0530

    Create ThirdSem.java

---
+++ JAVA OOPs LAB/4th Program/ThirdSem.java
@@ -0,0 +1,20 @@
+package CSE;
+
+public class ThirdSem {
+
+    public void message(){
+        System.out.println("Welcome to 3rd sem- Young budding engineers");
+    }
+
+    void defaultMethod(){
+        System.out.println("Default method is accessible");
+    }
+
+    protected void protectedMethod(){
+        System.out.println("Protected method is accessible");
+    }
+
+    private void privateMethod(){
+        System.out.println("Private method is accessible");
+    }
+}
