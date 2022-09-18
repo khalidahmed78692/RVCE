f6e0e710a4c348eb6bd38144428210726d6af4cc
Mohammed Khalid Mohammed Minhajuddin Ansari <khalidahmed78692@gmail.com>
2022-09-19 01:39:29 +0530

    Update 1A_1_1 c code corresponding assembly code.asm

--- MCES LAB/Part A/1A_1_1 c code corresponding assembly code.asm
+++ MCES LAB/Part A/1A_1_1 c code corresponding assembly code.asm
@@ -2,13 +2,14 @@
 	AREA RESET,CODE
 	ENTRY
 	
-	MOV R0,#00
-	MOV R1,#20
-	MOV R2,#14
-	MOV R3,#27
+	MOV R0,#00; -->A
+	MOV R1,#20; -->B
+	MOV R2,#14; -->C
+	MOV R3,#27; -->D
 	
 	;Arithematic operations (Addition and Subtraction)
 	ADD R0,R1,R2
 	SUB R0,R0,R3
 STOP B STOP
-	END
+	END ; This end is not necessary, if you don't write this you'll get a warning that END directive is missing, but it is not an error. Without END directive 
+	    ; your code runs fine.
