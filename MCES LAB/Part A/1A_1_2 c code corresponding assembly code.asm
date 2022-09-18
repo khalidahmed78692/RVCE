94073b8dc66f1a38441bd3305dcfcf08852bec7a
Mohammed Khalid Mohammed Minhajuddin Ansari <khalidahmed78692@gmail.com>
2022-09-19 01:50:46 +0530

    Update 1A_1_2 c code corresponding assembly code.asm

--- MCES LAB/Part A/1A_1_2 c code corresponding assembly code.asm
+++ MCES LAB/Part A/1A_1_2 c code corresponding assembly code.asm
@@ -7,3 +7,17 @@
 	ADD R1,R2,R1,LSL #1
 STOP B STOP
 	END
+
+;Using MUL directive
+
+;	AREA RESET,CODE
+;	ENTRY
+	
+;	MOV R0,#3; -->A
+;	MOV R1,#2; -->B
+;	MOV R2,#2; MULTIPLIER
+;	;ADD R0,R1,R0,LSL #1;
+;	MLA R3,R0,R2,R1
+	
+;STOP B STOP
+;	END
