f1efc5147f25b88886e0395fd482f90d0d17e271
Mohammed Khalid Mohammed Minhajuddin Ansari <khalidahmed78692@gmail.com>
2022-09-19 11:13:14 +0530

    Update 3b stepper motor.c

--- MCES LAB/Part B/3b stepper motor.c
+++ MCES LAB/Part B/3b stepper motor.c
@@ -1,10 +1,17 @@
 #include <lpc214x.h>
 
+#define LED_ON (IOCLR0=1U<<31)
+#define LED_OFF (IOSET0=1U<<31)
+#define PLOCK 0X00000400
+
+void SystemInit(void);
 void delay_ms(unsigned int j);
 
 int main(){
 	unsigned int no_of_steps_clk = 100, no_of_steps_aclk = 100;
-	IO0DIR |= 0x000F0000;
+	IO0DIR |= 0x000F0000|1u<<31;
+	LED_ON;delay_ms(250);LED_OFF;
+	SystemInit();
 	do{
 		IO0CLR = 0X000F0000;IO0SET = 0X00010000;delay_ms(10);if(--no_of_steps_clk == 0) break;
 		IO0CLR = 0X000F0000;IO0SET = 0X00020000;delay_ms(10);if(--no_of_steps_clk == 0) break;
