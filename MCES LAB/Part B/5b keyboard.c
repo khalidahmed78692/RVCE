657abb1faa3e187120a7771a15dceaa08d5061fd
Mohammed Khalid Mohammed Minhajuddin Ansari <khalidahmed78692@gmail.com>
2022-09-19 11:53:36 +0530

    Update 5b keyboard.c

--- MCES LAB/Part B/5b keyboard.c
+++ MCES LAB/Part B/5b keyboard.c
@@ -1,23 +1,27 @@
 #include <lpc214x.h>
 
+#define PLOCK 0x00000400
 #define LED_ON (IOSET0=0X80000000)
 #define COL0 (IOPIN1&1<<19)
 #define COL1 (IOPIN1&1<<18)
 #define COL2 (IOPIN1&1<<17)
 #define COL3 (IOPIN1&1<<16)
 
-unsigned char lookup_table[4][4]=   {{'0','1','2','3'},
-									{'4','5','6','7'},
-									{'8','9','a','b'},
-									{'c','d','e','f'}};
+unsigned char lookup_table[4][4]=       {{'0','1','2','3'},
+					{'4','5','6','7'},
+					{'8','9','a','b'},
+					{'c','d','e','f'}};
+
 void delay_ms(unsigned int);
 void uart_init(void);
+void SystemInit(void);
 
 int main(){
-	LED_ON;
+	SystemInit();
 	uart_init();
-	unsigned char rowsel,colsel;
 	IODIR0=0X000F000|1U<<31;
+	LED_ON;
+	unsigned char rowsel,colsel;
 	while(1){
 		while(1){
 			rowsel=0;IO0SET=0X000F0000;IO0CLR=1<<16;
